162e1e7aa3ef3914174e9d95aa182446142d66c5
J-K-M <90958043+J-K-M@users.noreply.github.com>
2025-12-13 12:53:50 +0000

    fixes for new pi OS changes

--- main.py
+++ main.py
@@ -10,17 +10,14 @@ import dotenv
 # to control servo motor
 import pigpio
 # to write to e-paper display
-
-REPO_PATH = "/home/ding/doorbell/"
-
 from PIL import Image,ImageDraw,ImageFont
-sys.path.append(REPO_PATH + "e-Paper/RaspberryPi_JetsonNano/python/lib/")
-from waveshare_epd import epd2in13_V4
+import epaper
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.WARNING)
 discord.VoiceClient.warn_nacl = False
 
+EP = epaper.epaper("epd2in13_V4")
 # GPIO 4, header pin 7
 SERVO_PIN = 4
 pwm = None
@@ -33,7 +30,7 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-FONT_FILE = REPO_PATH + "AtkinsonHyperlegibleMonoVF-Variable.ttf"
+FONT_FILE = "AtkinsonHyperlegibleMonoVF-Variable.ttf"
 FONT15 = ImageFont.truetype(FONT_FILE, size=15, encoding="unic")
 FONT15.set_variation_by_name("ExtraBold")
 FONT24 = ImageFont.truetype(FONT_FILE, size=24, encoding="unic")
@@ -47,7 +44,7 @@ def main():
     pwm.set_mode(SERVO_PIN, pigpio.OUTPUT)
     pwm.set_PWM_frequency(SERVO_PIN, 50)
 
-    epd = epd2in13_V4.EPD()
+    epd = EP.EPD()
     epd.init()
     display_message("d[•_•]b", "Listening for @doorbell mentions on Discord")
 
@@ -64,10 +61,10 @@ async def on_message(message: discord.Message):
     if user_mentioned("doorbell", message) or role_mentioned("doorbell", message):
         logger.info(f"{message.author.name} mentioned @doorbel [{message.clean_content}]")
 
-        ring_bell()
-        logger.debug(f"{message.author.nick=}, {message.author.name=} {message.author.global_name=}")
-        display_message(message.author.nick or message.author.global_name or message.author.name, message.clean_content)
-        await message.add_reaction("🔔")
+        with asyncio.TaskGroup() as tg:
+            t1 = tg.create_task(ring_bell())
+            t2 = tg.create_task(display_message(message.author.nick or message.author.global_name or message.author.name, message.clean_content))
+            t3 = tg.create_task(message.add_reaction("🔔"))
 
 
 def get_wrapped_text(text: str, font: ImageFont.ImageFont, line_length: int):
@@ -89,8 +86,8 @@ def user_mentioned(username: str, message: discord.Message) -> bool:
     return any([username == u.name for u in message.mentions])
 
 
-def display_message(heading: str, body: str):
-    epd.init()
+async def display_message(heading: str, body: str):
+    epd.init() # wake the display from sleep
     image = Image.new("1", (epd.height, epd.width), 255)
     draw = ImageDraw.Draw(image)
     draw.text((0, 0), heading, font=FONT24, fill=0)
@@ -101,9 +98,9 @@ def display_message(heading: str, body: str):
     epd.sleep()
 
 
-def ring_bell():
+async def ring_bell():
     pwm.set_servo_pulsewidth(SERVO_PIN, 500)
-    time.sleep(0.3)
+    asyncio.sleep(0.3)
     pwm.set_servo_pulsewidth(SERVO_PIN, 2500)
 
 
@@ -117,4 +114,4 @@ if __name__ == "__main__":
             pwm.set_PWM_frequency(SERVO_PIN, 0)
         if epd:
             display_message("(-.-) Zzz", "Sleeping")
-            epd2in13_V4.epdconfig.module_exit(cleanup=True)
+            EP.epdconfig.module_exit(cleanup=True)
